2395d23b5a5c353ed714192de4f6bb7f6f9de53f
Luis Mendoza <lmendoza.mg@gmail.com>
2021-02-01 09:29:23 +0100

    Sequence of numbers and animation

--- js/index.js
+++ js/index.js
@@ -6,14 +6,62 @@ const btnEmpezar = document.getElementById('btnEmpezar');
 
 class Juego {
   constructor() {
-    this.inicializar();
+    this.inicializar()
+    this.generarSecuencia()
+    this.siguienteNivel()
   }
   
   inicializar() {
-    btnEmpezar.classList.add('hide');
-  }        
+    btnEmpezar.classList.add('hide')
+    this.level = 1
+    this.colors = {
+    	celeste,
+    	violeta,
+		naranja,
+		verde
+    }
+  }
+
+  generarSecuencia() {
+  	this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+
+  }
+
+  siguienteNivel(){
+  	this.iluminarSecuencia()
+
+  }
+
+  transformarNumeroAColor(numero){
+  	switch (numero){
+  		case 0:
+  			return 'celeste'
+  		case 1:
+  			return 'violeta'
+  		case 2:
+  			return 'naranja'
+  		case 3:
+  			return 'verde'
+  	}
+  }
+
+  iluminarSecuencia(){
+  	for(let i = 0; i < this.level; i++){
+  		let color = this.transformarNumeroAColor(this.secuencia[i])
+  		setTimeout(() => this.iluminarColor(color), 1000 * i)
+  	}
+  }
+
+  iluminarColor(color){
+  	this.colors[color].classList.add('light')
+  	setTimeout(() => this.apagarColor(color), 350)
+  }
+
+  apagarColor(color){
+  	this.colors[color].classList.remove('light')
+  }
 }
 
 function empezarJuego() {
-  var juego = new Juego();
+  var juego = new Juego()
 }
